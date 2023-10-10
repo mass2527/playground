22863a06e2299bad0ae3eded071953fede526a1e
Dongho Kim <dongre2527@gmail.com>
2023-10-10 13:26:47 +0900

    Add content to PostDetailsPage

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,3 +1,31 @@
-export default function PostDetailsPage() {
-  return <div>PostDetailsPage</div>;
+import prisma from "@/lib/prisma";
+import { notFound } from "next/navigation";
+
+export default async function PostDetailsPage({
+  params,
+}: {
+  params: { postId: string };
+}) {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: params.postId,
+    },
+    include: {
+      author: true,
+    },
+  });
+  if (!post) {
+    notFound();
+  }
+  if (!post.published) {
+    <div>This post is not published</div>;
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <div>Posted by {post.author.username}</div>
+      <p>{post.content}</p>
+    </div>
+  );
 }
